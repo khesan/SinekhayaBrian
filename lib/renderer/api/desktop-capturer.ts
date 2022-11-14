2137ac4a84723281d4de10e89443aadd254d60fc
sinekhaya Brian ntuli <63958573+sinekhayantuli@users.noreply.github.com>
2022-11-14 07:39:08 +0200

    Initialize 10-x-y

---
+++ lib/renderer/api/desktop-capturer.ts
@@ -0,0 +1,18 @@
+import { ipcRendererInternal } from '@electron/internal/renderer/ipc-renderer-internal';
+import { deserialize } from '@electron/internal/common/type-utils';
+
+const { hasSwitch } = process.electronBinding('command_line');
+
+const enableStacks = hasSwitch('enable-api-filtering-logging');
+
+function getCurrentStack () {
+  const target = {};
+  if (enableStacks) {
+    Error.captureStackTrace(target, getCurrentStack);
+  }
+  return (target as any).stack;
+}
+
+export async function getSources (options: Electron.SourcesOptions) {
+  return deserialize(await ipcRendererInternal.invoke('ELECTRON_BROWSER_DESKTOP_CAPTURER_GET_SOURCES', options, getCurrentStack()));
+}
