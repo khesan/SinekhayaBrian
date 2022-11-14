837b153a0a6d5cf33fff00de4bef4e05f461211d
sinekhaya Brian ntuli <63958573+sinekhayantuli@users.noreply.github.com>
2022-11-14 07:35:58 +0200

    Initialize 6-1-x

---
+++ lib/renderer/ipc-renderer-internal.ts
@@ -0,0 +1,24 @@
+const binding = process.electronBinding('ipc')
+const v8Util = process.electronBinding('v8_util')
+
+// Created by init.js.
+export const ipcRendererInternal: Electron.IpcRendererInternal = v8Util.getHiddenValue(global, 'ipc-internal')
+const internal = true
+
+if (!ipcRendererInternal.send) {
+  ipcRendererInternal.send = function (channel, ...args) {
+    return binding.ipc.send(internal, channel, args)
+  }
+
+  ipcRendererInternal.sendSync = function (channel, ...args) {
+    return binding.ipc.sendSync(internal, channel, args)[0]
+  }
+
+  ipcRendererInternal.sendTo = function (webContentsId, channel, ...args) {
+    return binding.ipc.sendTo(internal, false, webContentsId, channel, args)
+  }
+
+  ipcRendererInternal.sendToAll = function (webContentsId, channel, ...args) {
+    return binding.ipc.sendTo(internal, true, webContentsId, channel, args)
+  }
+}
